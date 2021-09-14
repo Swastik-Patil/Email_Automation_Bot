8a2b568fda7b838e8e8c37a7b52d26f67e597f08
Swastik Patil <79092900+MrSwastik592@users.noreply.github.com>
2021-09-14 16:58:49 +0530

    Add files via upload

---
+++ Email_Automaition_Bot.py
@@ -0,0 +1,66 @@
+import smtplib
+import speech_recognition as sr
+import pyttsx3
+from email.message import EmailMessage
+
+listener = sr.Recognizer()
+engine = pyttsx3.init()
+
+
+def talk(text):
+    engine.say(text)
+    engine.runAndWait()
+
+
+def get_info():
+    try:
+        with sr.Microphone() as source:
+            print('Listening...')
+            voice = listener.listen(source)
+            info = listener.recognize_google(voice)
+            print(info)
+            return info.lower()
+    except:
+        pass
+
+
+def send_email(receiver, subject, message):
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.starttls()
+    server.login("1901290222.swastik.patil@gmail.com", "SONU9698")
+    email = EmailMessage()
+    email['From'] = "1901290222.swastik.patil@gmail.com"
+    email['To'] = receiver
+    email['Subject'] = subject
+    email.set_content(message)
+    server.send_message(email)
+
+
+email_list = {
+    'sonu': 'swastik592@gmail.com',
+    'swastik': 'swastik9698@gmail.com',
+}
+
+
+def get_email_info():
+    talk('To Whom you want to send email')
+    name = get_info()
+    receiver = email_list[name]
+    print(receiver)
+    talk('What is the subject of your email?')
+    subject = get_info()
+    talk('Tell me the text in your email')
+    message = get_info()
+
+    talk('Do you want to send this mail ?')
+    send_this = get_info()
+    if 'yes' in send_this:
+        send_email(receiver, subject, message)
+        talk('Hey lazy ass. Your email is sent')
+        talk('Do you want to send more email?')
+        send_more = get_info()
+        if 'yes' in send_more:
+            get_email_info()
+
+
+get_email_info()
